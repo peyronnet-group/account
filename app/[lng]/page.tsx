548f43e207f73fc6c542e30b55f158458e627f65
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-05-18 14:45:54 +0200

    Added new background in landing page

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,26 +1,38 @@
 "use client";
-import Image from "next/image";
 import { useTranslation } from "../i18n/client";
 import { Paragraph, Title } from "@/components/text";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import SiteFooter from "@/components/footer";
 import Logo from "@/components/logo";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import { motion } from "framer-motion";
 
 export default function Home({ params: { lng } }: { params: { lng: any } }) {
   const { t } = useTranslation(lng, "common");
 
   return (
     <>
-      <main className="min-h-screen p-2 flex flex-col justify-center">
-        <Title className="text-center">{t("home-headline")}</Title>
-        <Paragraph className="text-center">{t("home-desc")}</Paragraph>
-        <span className="flex justify-center m-2">
-          <Link href="/login">
-            <Button variant="outline">{t("sign-in")}</Button>
-          </Link>
-        </span>
+      <main className="min-h-screen flex flex-col justify-center">
+        <AuroraBackground>
+          <motion.div
+            initial={{ opacity: 0.0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className="relative flex flex-col gap-4 items-center justify-center px-4 dark:text-white"
+          >
+            <Title className="text-center">{t("home-headline")}</Title>
+            <Paragraph className="text-center">{t("home-desc")}</Paragraph>
+            <span className="flex justify-center m-2">
+              <Link href="/login">
+                <Button variant="outline">{t("sign-in")}</Button>
+              </Link>
+            </span>
+          </motion.div>
+        </AuroraBackground>
       </main>
       <footer className="flex flex-col justify-center space-y-2 px-5 py-10 sm:grid sm:grid-cols-3">
         <div className="flex items-center justify-center sm:justify-normal">
