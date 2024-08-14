49068a02f0169f9c7d90f04bbea8b38b9d432cc7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-08-14 18:29:14 +0200

    Updated URL

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
     if (error) {
       return NextResponse.redirect(
         getErrorRedirect(
-          `${requestUrl.origin}/signin`,
+          `https://account.peyronnet.group/signin`,
           error.name,
           "Sorry, we weren't able to log you in. Please try again."
         )
@@ -28,7 +28,7 @@ export async function GET(request: NextRequest) {
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(
     getStatusRedirect(
-      `${requestUrl.origin}/me`,
+      `https://account.peyronnet.group/me`,
       "Success!",
       "You are now signed in."
     )
--- app/auth/reset_password/route.ts
+++ app/auth/reset_password/route.ts
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
     if (error) {
       return NextResponse.redirect(
         getErrorRedirect(
-          `${requestUrl.origin}/signin/forgot_password`,
+          `https://account.peyronnet.group/signin/forgot_password`,
           error.name,
           "Sorry, we weren't able to log you in. Please try again."
         )
@@ -28,7 +28,7 @@ export async function GET(request: NextRequest) {
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(
     getStatusRedirect(
-      `${requestUrl.origin}/signin/update_password`,
+      `https://account.peyronnet.group/signin/update_password`,
       "You are now signed in.",
       "Please enter a new password for your account."
     )
