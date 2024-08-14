20cffa04d69f03dc59756610c3710e6825a15686
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-08-14 13:16:28 +0200

    Updated Separator design

--- components/ui/AuthForms/Separator.tsx
+++ components/ui/AuthForms/Separator.tsx
@@ -6,11 +6,11 @@ export default function Separator({ text }: SeparatorProps) {
   return (
     <div className="relative">
       <div className="relative flex items-center py-1">
-        <div className="grow border-t border-zinc-700"></div>
-        <span className="mx-3 shrink text-sm leading-8 text-zinc-500">
+        <div className="grow border-t border-slate-700"></div>
+        <span className="mx-3 shrink text-sm leading-8 text-slate-500">
           {text}
         </span>
-        <div className="grow border-t border-zinc-700"></div>
+        <div className="grow border-t border-slate-700"></div>
       </div>
     </div>
   );
