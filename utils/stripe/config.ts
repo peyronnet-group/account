1568aa19d97a2f0a49f082df36741ec4aa2886d7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-01-23 13:07:52 +0100

    Version 2.1.3

--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -11,7 +11,7 @@ export const stripe = new Stripe(
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
       name: "GRP Account",
-      version: "2.1.2",
+      version: "2.1.3",
       url: "https://github.com/peyronnet-group/account",
     },
   },
